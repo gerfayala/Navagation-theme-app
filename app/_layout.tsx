081bd9c4e2bfc7d7a92ba845c80f136362dd739b
German <germanfayala2419@gmail.com>
2024-11-24 19:13:40 -0400

    feat(init-001): Create base project

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Slot } from "expo-router";
+
+import "../global.css";
+
+const MainLayout = () => {
+  return <Slot />;
+};
+
+export default MainLayout;
---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View className="flex flex-1 bg-neutral-300">
+      <Text className="color-black text-center font-semibold text-lg  ">
+        App
+      </Text>
+    </View>
+  );
+};
+
+export default App;
